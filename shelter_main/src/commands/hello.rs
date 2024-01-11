50311b09c9d872ff08f9a93913bf64ae5d3a0f5e
Changyou Liang <changyou.liang@shopee.com>
2024-01-11 17:01:29 +0800

    commands

---
+++ shelter_main/src/commands/hello.rs
@@ -0,0 +1,12 @@
+use clap::{ArgMatches, Command};
+
+pub fn configure() -> Command {
+    Command::new("hello").about("Hello World!")
+}
+
+pub fn handle(matches: &ArgMatches) -> anyhow::Result<()> {
+    if let Some(_matches) = matches.subcommand_matches("hello") {
+        println!("Hello World!");
+    }
+    Ok(())
+}
--- shelter_main/src/main.rs
+++ shelter_main/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Arg, Command};
+use anyhow;
+use dotenv::dotenv;
+
+use shelter_main::commands;
+
+pub fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+
+    let command = Command::new("Dog Shelter  sample application")
+        .version("1.0")
+        .author("liangchangyoujackson@gmail.com")
+        .about("A sample application to experiment with Rust-based microservices")
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .long("config")
+                .help("Configuration file location")
+                .default_value("config.json")
+        );
+
+    let command = commands::configure(command);
+    let _matches = command.get_matches();
+    commands::handle(&_matches)?;
+    Ok(())
 }
