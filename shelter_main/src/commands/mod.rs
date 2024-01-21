bc456be56ed9adfb24194fd6bdf1313ddc73633c
jacksonyoudi <liangchangyoujackson@gmail.com>
2024-01-21 23:15:47 +0800

    createadmin

--- shelter_main/src/commands/mod.rs
+++ shelter_main/src/commands/mod.rs
@@ -4,19 +4,21 @@ use crate::settings::Settings;
 
 mod hello;
 mod serve;
-
 mod migrate;
+mod createadmin;
 
 pub fn configure(command: Command) -> Command {
     command
         .subcommand(hello::configure())
         .subcommand(serve::configure())
         .subcommand(migrate::configure())
+        .subcommand(createadmin::configure())
 }
 
 pub fn handle(matches: &ArgMatches, _settings: &Settings) -> anyhow::Result<()> {
     hello::handle(matches, _settings)?;
     serve::handle(matches, _settings)?;
     migrate::handle(matches, _settings)?;
+    createadmin::handle(matches, _settings)?;
     Ok(())
 }
